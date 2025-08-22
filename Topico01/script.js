fb50eb2c2b026a2cbbca12d6b02bef9c9d26c854
Matheus <113068587+mathewsph2@users.noreply.github.com>
2025-08-22 00:19:04 -0300

    Add files via upload

---
+++ Topico01/script.js
@@ -0,0 +1,27 @@
+// Exercício do tópico 01
+
+
+// 1- Declarar uma variável e atribuir o seu nome;
+var nome = "Matheus";
+console.log(nome);
+
+
+// 2- Declarar uma variável e atribuir sua idade;
+var idade = 30;
+console.log(idade);
+
+// 3- Declarar uma variável e atribuir sua comida favorita;
+var comidaFavorita = "Churrasco";
+console.log(comidaFavorita);
+
+// 4- Declarar 3 variáveis sem valor;
+var a;
+console.log(a);
+
+
+var b;
+console.log(b);
+
+
+var c;
+console.log(c);
