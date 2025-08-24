c6a83596524b0d4969b191a9b1b3373342a23f03
Matheus <113068587+mathewsph2@users.noreply.github.com>
2025-08-24 19:10:18 -0300

    Add files via upload

---
+++ Topico02/Exercicio1/script.js
@@ -0,0 +1,39 @@
+// Exercício 1 do tópico 02
+
+
+// Declare uma variável contendo uma string
+var nome = 'Matheus';
+console.log(nome);
+
+var sobrenome = 'Ribeiro';
+console.log(sobrenome);
+
+
+// Declare uma variável contendo um número dentro de uma string
+var frase = 'O aluno fez 10 pontos';
+console.log(frase);
+
+
+// Declare uma variável com a sua idade
+var idade = 35;
+console.log(idade);
+
+// Declare outra variável para conter a frase: Meu nome é SEU NOME e minha idade é SUA IDADE
+var dadospessoais = `Meu nome é ${nome} e minha idade é ${idade}!`
+// Template String: Dentro de crases
+console.log(dadospessoais);
+
+
+// Declare outras duas variáveis, uma com seu sobrenome e com seu nome exiba as mesmas juntas
+var nomecompleto = nome + ' ' + sobrenome;
+console.log(nomecompleto);
+
+
+// Coloque a seguinte frase em uma variável: It's study time
+var frase = "It's study time!"
+console.log(frase);
+
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
+
